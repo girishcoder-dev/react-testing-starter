fd88380007061cb3188d0ced97647ce6bc5591c2
Girish Suri <girishgeneral@gmail.com>
2025-12-20 17:55:11 +0530

    Exercise - Testing ExpandableTest

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,8 +1,9 @@
-import Onboarding from "../components/Onboarding";
-import TermsAndConditions from "../components/TermsAndConditions";
+import ExpandableText from "../components/ExpandableText";
 
 const PlaygroundPage = () => {
-  return <TermsAndConditions />;
+  return (
+    <ExpandableText text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet veniam, labore at fuga odit iure eum corrupti natus aliquid ducimus vitae deleniti in laborum placeat velit veritatis facere nesciunt perferendis ipsa! Similique minima illum numquam odio modi culpa ratione nobis magnam omnis commodi nesciunt ut nisi ducimus, quasi natus, quisquam doloribus ab animi dolor minus unde quas nostrum totam. Rem vitae ipsa, voluptates neque, distinctio sed ad quo ipsam est doloremque molestiae optio, dolorem sint dicta corrupti ratione magnam laudantium explicabo quod nihil! Perspiciatis, molestias. Rerum quis, placeat error recusandae perferendis nam aspernatur minus fuga sint! Similique, dignissimos tempora? Laboriosam." />
+  );
 };
 
 export default PlaygroundPage;
